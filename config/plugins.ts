c8e4a14f77dd42a8608deabdd6170de97b4cdec4
Nguyen Van Thai <vanthai2704@gmail.com>
2026-01-16 09:07:20 +0700

    thêm user table

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env }: { env: any }) => ({
+  // Các plugin khác nếu có...
+
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: ["fullname"], // Thêm fullname (và các field custom khác nếu cần, ví dụ: ['fullname', 'phone', 'birthdate'])
+      },
+    },
+  },
+});
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1155,7 +1155,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -1169,6 +1168,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    fullname: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
